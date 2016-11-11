813b797b34693aa1d7e8cc93ba1130b0dab10805
Lance-Inman <littlevolt@gmail.com>
2016-11-11 15:56:44 -0800

    Add ID3Classifier class

---
+++ src/src/ID3Classifier.java
@@ -0,0 +1,11 @@
+/**
+ * A machine learning algorithm to classify objects in a given data set
+ * using Iterative Dichotomiser 3
+ *
+ * @author  Lance Inman
+ * @version 1.0
+ * @see Classifier, Sample, DataSet
+ */
+public class ID3Classifier implements Classifier {
+
+}
